ee7a03d84acf1ae6e33a866f55222b79f8cd4b30
shvmpz05 <martinkimani914@gmail.com>
2023-02-22 09:23:33 +0000

    Betty outta my life

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,49 @@
 #include "main.h"
-
 /**
- * times_table -  print out time table of nine
+ * print_times_table -  print out time table of nine
+ * @n: integer to check
  *
  * Return: nothing
  */
-
 void print_times_table(int n)
 {
 	int a, b, prod;
-	
-	if ( n > 15 || n < 0)
+
+	if (n > 15 || n < 0)
 		;
 	else
-	{
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			prod = a * b;
-			if (prod >= 10 && prod < 100)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(' ');
-				_putchar('0' + (prod / 10));
-				_putchar('0' + (prod % 10));
-			}
-			else if (prod > 99)
-			{
+				prod = a * b;
+				if (prod >= 10 && prod < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+				}
+				else if (prod > 99)
+				{
 				_putchar('0' + (prod / 100));
 				_putchar('0' + ((prod / 10) % 10));
 				_putchar('0' + (prod % 10));
+				}
+				else if (prod < 10 && b != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				}
+				else
+					_putchar('0' + prod);
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			else if (prod < 10 && b != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + prod);
-			}
-			else
-				_putchar('0' + prod);
-
-			if (b != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-	_putchar('\n');
-	}
 	}
 }
