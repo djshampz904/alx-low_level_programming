8956483f00449ca7900e4e6cdaa4dde87da8d5b0
shvmpz05 <martinkimani914@gmail.com>
2023-02-22 16:54:49 +0000

    Pushing

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = 972;
-        b = 2000;
-        c = 1000;
+        a = -20;
+        b = -2;
+        c = -1;
 
         largest = largest_number(a, b, c);
 
