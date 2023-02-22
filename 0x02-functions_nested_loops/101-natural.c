6ce12d58aa5d991fa722e8598fe0abd2286b2b4f
shvmpz05 <martinkimani914@gmail.com>
2023-02-22 09:33:58 +0000

    My natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+	int sum, a;
+
+	for (a = 0; a < 1024; a++)
+		if ((a % 3) == 0 || (a % 5) == 0)
+			sum += a;
+		else
+			;
+	printf("%d\n", sum);
+}
