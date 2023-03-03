2954c08ab7ea01e4e853a2397a0ef511c1828ad8
shvmpz05 <martinkimani914@gmail.com>
2023-03-03 07:26:53 +0000

    $1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,20 +9,21 @@
 
 char *cap_string(char *s)
 {
-	int i, found;
+	int i, found = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '\?' ||
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '?' ||
 			s[i] == '"' || s[i] == '(' || s[i] == '.' || s[i] == ')'
 			|| s[i] == '}' || s[i] == '{' || s[i] == '!' || s[i] == ';'
 			|| s[i] == ',')
 			found = 0;
-		else if (found == 0 && s[i] <= 122 && s[i] >= 97)
+		else if (found == 0 && s[i] >= 97 && s[i] <= 122)
 		{
 			s[i] = s[i] - 32;
 			found = 1;
 		}
-		else if (((s[i] <= 89 && s[i] >= 65) || (s[i] <= 57 && s[i] >= 48)) && found == 0)
+		else if (((s[i] <= 89 && s[i] >= 65) ||
+			(s[i] <= 57 && s[i] >= 48)) && found == 0)
 			found = 1;
 	return (s);
 }
