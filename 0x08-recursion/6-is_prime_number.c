98a41b6afd467cac8963a7d8adb0affb50d0b815
shvmpz05 <martinkimani914@gmail.com>
2023-03-08 17:38:16 +0000

    $message

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * is_prime_number - check if number is a prime number
+ * helper - check if number is a prime number
  *
  * @n: number to check
+ * @i: number to check
  * Return: return integer
  */
 
 int helper(int i, int n)
 {
-	if (n % i != 0)
-	{
-		if (i > n)
-			return (1);
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
 		return (helper(i + 1, n));
-	}
-	return (0);
+	return (1);
 }
+
+/**
+ * is_prime_number - check if its prime number
+ *
+ * @n: integer to check
+ * Return: integer
+ */
 
 int is_prime_number(int n)
 {
