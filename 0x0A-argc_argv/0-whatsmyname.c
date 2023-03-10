59477382ee2dc398c59951e68a70d9cc8a158af9
shvmpz05 <martinkimani914@gmail.com>
2023-03-10 11:51:12 +0000

    mymessage

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of characters
+ * Return: 0 Always success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
