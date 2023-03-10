b1f4fdefc74ea2ae1e4bbdaaf161a757fbd143fa
shvmpz05 <martinkimani914@gmail.com>
2023-03-10 12:42:04 +0000

    last push

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: array of argumnets
+ * Return: 0 always Succes
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
