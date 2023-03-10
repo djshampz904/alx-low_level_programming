5e78fc4374dab2899d8726deba6cd49956b08876
shvmpz05 <martinkimani914@gmail.com>
2023-03-10 16:23:10 +0000

    correction add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,10 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc == 1)
+	{
 		printf("%d\n", 0);
+		return (1);
+	}
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
@@ -31,6 +34,6 @@ int main(int argc, char *argv[])
 			}
 		}
 		printf("%d\n", sum);
+		return (0);
 	}
-	return (0);
 }
