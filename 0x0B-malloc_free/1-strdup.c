7221bf76c5adbe49ca8b1597e06fd2c7e33a6466
shvmpz05 <martinkimani914@gmail.com>
2023-03-14 09:24:50 +0000

    $message

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,10 +13,10 @@ char *_strdup(char *str)
 	unsigned int i, len;
 	char *ptr;
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
 	if (str == NULL)
 		return (0);
+	for (len = 0; str[len] != '\0'; len++)
+		;
 	ptr = (char *) malloc(sizeof(char) * len + 1);
 	if (ptr == NULL)
 		return (0);
