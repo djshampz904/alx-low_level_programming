6e0384387365aa241cc899b12ff246f253b0a4b9
shvmpz05 <martinkimani914@gmail.com>
2023-03-23 15:23:44 +0000

    correction on printing name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints name to std output
@@ -9,5 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
