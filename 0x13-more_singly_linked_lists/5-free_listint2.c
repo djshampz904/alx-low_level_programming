91596328de205ffd17562d7198465bc54cd91a06
djshampz904 <djshampz05@gmail.com>
2023-04-05 11:48:49 +0000

    freememory and assign head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free memory and set head to null
+ * @head: pointer to head pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *current;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+
+		free(temp);
+	}
+	*head = NULL;
+}
