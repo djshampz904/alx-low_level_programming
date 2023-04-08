cedc93bf3133315145c83967ed6f800a2942c018
djshampz904 <djshampz05@gmail.com>
2023-04-08 11:45:12 +0000

    correctin

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,6 +16,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	num = sizeof(n) * 8 - 1;
 	i <<= ((sizeof(n) * 8) - 1);
 
+	if (index >= sizeof(n) * 8)
+		return (-1);
 	while (i > 0)
 	{
 		if (num == index)
