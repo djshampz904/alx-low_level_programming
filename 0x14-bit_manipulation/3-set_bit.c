d1abc4a2d68628eb7376b7dc2f36cd2768d389aa
djshampz904 <djshampz05@gmail.com>
2023-04-08 12:07:02 +0000

    setting bit at a certain position

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit at certain index to a 1
+ * @n: pointer to n;
+ * @index: postition to change
+ * Return: integer
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i, num;
+
+	i = 1;
+	num = sizeof(n) * 8 - 1;
+	i <<= ((sizeof(n) * 8) - 1);
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	while (i > 0)
+	{
+		if (num == index)
+		{
+			*n |= i;
+		}
+		i = i >> 1;
+		num--;
+	}
+	return (*n);
+}
