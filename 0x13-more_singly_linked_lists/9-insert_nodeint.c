7e4a5803111d047b958242751684c28d93148d83
djshampz904 <djshampz05@gmail.com>
2023-05-02 08:02:38 +0000

    insert at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - add node at index
+ * @head: pointer to head of list
+ * @idx: index position
+ * @n: number to insert
+ * Return: node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *current;
+	unsigned int i;
+
+	i = 0;
+	if (head == NULL)
+		return (0);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return new_node;
+	}
+
+	current = *head;
+	while (i < idx - 1 && current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	if (current == NULL && idx != 0)
+	{
+		free(new_node);
+		return NULL;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return new_node;
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define lists_h
 
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -33,5 +33,7 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 int sum_listint(listint_t *head);
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 #endif
