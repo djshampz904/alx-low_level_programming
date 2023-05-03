dee5cb10b7359abd63347d2263ff286c79feddb1
martin-maverick <97390728+djshampz904@users.noreply.github.com>
2023-05-04 00:30:38 +0300

    Add files via upload

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,26 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * struct dog - structure of a dog
- *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - dog type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
-int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
