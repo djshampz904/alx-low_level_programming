b64c9b97a494db0f799e49b427080febde836222
djshampz904 <djshampz05@gmail.com>
2024-01-11 14:03:32 +0300

    betty-style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,15 @@
  * Return: number of nodes in the list
  */
 
-size_t print_dlistint(const dlistint_t *h){
-    size_t count = 0;
-    while (h != NULL){
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-    return (count);
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
