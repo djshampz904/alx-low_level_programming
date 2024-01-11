cad94ead9aae7463e58e797dbf62f5b967611757
djshampz904 <djshampz05@gmail.com>
2024-01-11 14:40:29 +0300

    add element at the end

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -19,5 +19,7 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 /*function to add a new node at the beginning of a dlistint_t list*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/*function to add a node at the end of a dlistint_t list*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif
