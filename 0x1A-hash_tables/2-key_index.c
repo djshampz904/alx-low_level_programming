cf446277fdbbed882996a0d9edc9ecba955acac4
djshampz904 <djshampz05@gmail.com>
2024-02-02 15:34:49 +0300

    betty doc

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - function that gives you the index of a key
  * @key: key to find index of
  * @size: size of the array of the hash table
+ * Return: index at which the key/value pair should be stored in the array
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -14,6 +14,7 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
+
 typedef struct hash_node_s
 {
 	char *key;
@@ -29,6 +30,7 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
+
 typedef struct hash_table_s
 {
 	unsigned long int size;
@@ -42,6 +44,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
 
 #endif
